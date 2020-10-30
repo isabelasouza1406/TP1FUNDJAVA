880e6aa3da907b49745939928a8a03ee60406d46
Isabela <isabela.souza@al.infnet.edu.br>
2020-10-30 00:36:20 -0300

    Finalizacao do TP

--- src/com/edu/infnet/Aluno.java
+++ src/com/edu/infnet/Aluno.java
@@ -1,46 +1,60 @@
 package com.edu.infnet;
+
 // Definição da classe Aluno
 public class Aluno {
 
-
 	private String cpf;
-	private Float[] notas = new Float[5];
+	private Float[] notas = new Float[2];
 	private String nome;
-	
-	public Aluno (String nome, String cpf) {
+
+	public Aluno(String nome, String cpf) {
 		this.nome = nome;
 		this.cpf = cpf;
 	}
-	
+
 	public void registrarNota(Float nota) {
-		for(int i=0; i<4; i++) {
-			if (notas[i]== null) {
-				this.notas[i]=nota;
+		for (int i = 0; i < notas.length; i++) {
+			if (notas[i] == null) {
+				this.notas[i] = nota;
+				break;
 			}
-			
+
 		}
-		
+
+	}
+
+	public void calcularMedia() {
+		int totalDeNotas = 2;
+		Float nota1 = notas[0];
+		Float nota2 = notas[1];
+		Float media = (nota1+nota2)/totalDeNotas;
+		String situacao = media>7? "Aprovado":"Reprovado";
+		System.out.println("A média do aluno " + this.nome + " é: "+ media + ". Portanto o aluno está: " + situacao + ".");
 		
 	}
-	
-	
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Float[] getNotas() {
 		return notas;
 	}
+
 	public void setNotas(Float[] notas) {
 		this.notas = notas;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 }
--- src/com/edu/infnet/Application.java
+++ src/com/edu/infnet/Application.java
@@ -14,9 +14,9 @@ public class Application {
 		while (executando) {
 			Scanner sc = new Scanner(System.in);
 
-			System.out.println("1 - Registrar as notas do aluno");
-			System.out.println("2 - Consultar boletim do aluno");
-			System.out.println("3 - Consultar notas da turma");
+			System.out.println("1 - Registrar as notas do aluno.");
+			System.out.println("2 - Consultar boletim do aluno.");
+			System.out.println("3 - Consultar notas da turma.");
 			System.out.println("4 - Sair");
 
 			int opcaoSelecionada = sc.nextInt();
@@ -25,9 +25,8 @@ public class Application {
 			case (1):
 				// cadastrar aluno
 				System.out.println("Informe um nome");
-				System.out.println("Informe uma matricula");
-
 				String nome = sc.next();
+				System.out.println("Informe uma matricula");
 				String cpf = sc.next();
 				Aluno novoAluno = new Aluno(nome, cpf);
 
@@ -39,27 +38,25 @@ public class Application {
 				Float nota2 = sc.nextFloat();
 				novoAluno.registrarNota(nota2);
 
-				System.out.println("nota 3");
-				Float nota3 = sc.nextFloat();
-				novoAluno.registrarNota(nota3);
-
-				System.out.println("nota 4");
-				Float nota4 = sc.nextFloat();
-				novoAluno.registrarNota(nota4);
-
-				System.out.println("nota 5");
-				Float nota5 = sc.nextFloat();
-				novoAluno.registrarNota(nota5);
-
 				classe2b.add(novoAluno);
-				
 
 				break;
 
 			case (2):
+				System.out.println("Digite o CPF do aluno");
+				String pesquisaCpf = sc.next();
+				for (Aluno aluno : classe2b) {
+					if (aluno.getCpf().equals(pesquisaCpf)) {
+						aluno.calcularMedia();
+						
+					}
+				}
 				break;
 
 			case (3):
+				for (Aluno aluno : classe2b) {
+					aluno.calcularMedia();
+				}
 				break;
 
 			case (4):
@@ -69,11 +66,11 @@ public class Application {
 			}
 
 		}
-		for(Aluno aluno:classe2b) {
+		for (Aluno aluno : classe2b) {
 			System.out.println(aluno.getNome());
-			
-			Float notas [] = aluno.getNotas();
-			for(int i = 0; i<4; i++) {
+
+			Float notas[] = aluno.getNotas();
+			for (int i = 0; i < 2; i++) {
 				System.out.println(notas[i]);
 			}
 		}
